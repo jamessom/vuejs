5808c0e6ecc6d54960373e84aba12bc6e51b2ac6
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2017-12-12 00:18:14 -0200

    First samples, v-model and v-for

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var hello = new Vue({
+  el: '#hello',
+  data: {
+    msg: "hello VueJS!",
+    frameworks: [
+      {name: "Ruby on Rails"},
+      {name: "Slim"},
+      {name: "Codeigniter"},
+      {name: "VueJS"},
+      {name: "AngularJS"},
+      {name: "React"},
+      {name: "BackboneJS"},
+      {name: "MeteorJS"},
+      {name: "Zend Framework"},
+    ]
+  }
+});
